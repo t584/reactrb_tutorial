3931c732e90c225d4957638d3283c0be470bf45a
Tony Schopen <tschopen@gmail.com>
2016-10-16 08:17:56 -0700

    Created Record resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :records
   get 'home/search_path'
   mount ReactiveRecord::Engine => '/rr'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
