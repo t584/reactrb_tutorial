30223c8367e6f84c311cee9edabfdb6c49aef4b1
Tony <tschopen@gmail.com>
2016-10-28 12:08:07 -0700

    Add files via upload

---
+++ application_policy.rb
@@ -0,0 +1,6 @@
+# app/policies/application_policy.rb
+class ApplicationPolicy
+  always_allow_connection
+  regulate_all_broadcasts { |policy| policy.send_all }
+  allow_change(to: :all, on: [:create, :update, :destroy]) { true }
+end if Rails.env == 'development'
