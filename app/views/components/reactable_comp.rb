6783da3a8db8e2760bd4f3861d1998b226a09a1c
Tony Schopen <tschopen@gmail.com>
2016-11-17 11:33:44 -0800

    Reactable table example cleanup.

--- app/views/components/reactable_comp.rb
+++ app/views/components/reactable_comp.rb
@@ -1,23 +1,16 @@
 class ReactableExample < React::Component::Base
   before_mount do
     state.records!( [
-                         {name: "SG-1", leader: "Oneil", assignment: "Exploration", members: 4},
-                         {name: "SG-2", leader: "Kawalsky", assignment: "Search and Rescue", members: 5},
-                         {name: "SG-3", leader: "Reynolds", assignment: "Marine Combat", members: 10},
-                         {name: "SG-4", leader: "Howe", assignment: "Medical", members: 4},
-                         {name: "SG-5", leader: "Davis", assignment: "Marine Combat", members: 6},
-                         {name: "SG-6", leader: "Fischer", assignment: "Search and Rescue", members: 10},
-                         {name: "SG-7", leader: "Isaacs", assignment: "Scientific", members: 6},
-                         {name: "SG-8", leader: "Yip", assignment: "Medical", members: 6},
-                         {name: "SG-9", leader: "Winters", assignment: "Diplomatic", members: 7},
-                         {name: "SG-10", leader: "Colville", assignment: "Military Exploration", members: 5}
-                     ] )
-
-
-
-    state.record_1!({ Name: 'Griffin Smith', Age: 18})
-    state.record_2!({ Name: 'Lee Salminen', Age: 23 })
-    state.record_3!({ Name: 'Tyle Apple', Age: 28 })
+                      {name: "SG-1", leader: "Oneil", assignment: "Exploration", members: 4},
+                      {name: "SG-2", leader: "Kawalsky", assignment: "Search and Rescue", members: 5},
+                      {name: "SG-3", leader: "Reynolds", assignment: "Marine Combat", members: 10},
+                      {name: "SG-4", leader: "Howe", assignment: "Medical", members: 4},
+                      {name: "SG-5", leader: "Davis", assignment: "Marine Combat", members: 6},
+                      {name: "SG-6", leader: "Fischer", assignment: "Search and Rescue", members: 10},
+                      {name: "SG-7", leader: "Isaacs", assignment: "Scientific", members: 6},
+                      {name: "SG-8", leader: "Yip", assignment: "Medical", members: 6},
+                      {name: "SG-9", leader: "Winters", assignment: "Diplomatic", members: 7},
+                      {name: "SG-10", leader: "Colville", assignment: "Military Exploration", members: 5} ] )
   end
 
   def render
