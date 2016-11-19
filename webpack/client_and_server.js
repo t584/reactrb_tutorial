e72adc204282e36e3112a5080dd2499f4000ce04
Tony Schopen <tschopen@gmail.com>
2016-11-19 08:39:46 -0800

    Remove reactable package and require.

--- webpack/client_and_server.js
+++ webpack/client_and_server.js
@@ -2,5 +2,4 @@ ReactDOM = require('react-dom');
 React = require('react');
 ReactBootstrap = require('react-bootstrap');
 ReactPlayer = require('react-player');
-Reactable = require('reactable');
 console.log('client_and_server.js loaded');
