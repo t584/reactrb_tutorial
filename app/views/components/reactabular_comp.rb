5f151916023e2ad6bde424c8545588434c16328f
Tony Schopen <tschopen@gmail.com>
2016-11-23 22:23:19 -0800

    Header row added.

--- app/views/components/reactabular_comp.rb
+++ app/views/components/reactabular_comp.rb
@@ -10,32 +10,7 @@ class ReactabularExample < React::Component::Base
 
   def render
       Reactabular::Table::Provider( class: 'pure-table pure-table-striped', columns: state.columns.to_n ) {
+        Reactabular::Table::Header()
         Reactabular::Table::Body( rows: state.rows.to_n, row_key: :id ) }
   end
 end
-
-
-# class ReactabularExample < React::Component::Base
-#   before_mount do
-#     state.countries!({ fi: 'Finland', dk: 'Denmark' })
-#     state.rows!( [ { id: 100, name: 'John', tools: { hammer: true }, country: 'fi'},
-#                    { id: 101, name: 'Jack', tools: { hammer: false}, country: 'dk'} ] )
-#     state.col_1!( { property: 'name', header: { label: 'Name',
-#                         transforms: [ label.on(:click) { alert("clicked ${label}") } ] } } )
-#     state.col_2!( { property: 'tools', header: { label: 'Active',
-#                         transforms: [ label.on(:click) { alert("clicked ${label}") } ] },
-#                     cell: { format: (tools.hammer ? 'Hammertime' : 'nope') } } )
-#     state.col_3!( { property: 'country', header: { label: 'Country',
-#                         transforms: [ label.on(:click) { alert("clicked ${label}") } ] },
-#                     cell: { format: state.countries[country] } } )
-#     state.columns!([ state.col_1, state.col_2, state.col_3 ])
-#   end
-#
-#   def render
-#     div {
-#       # 'TEST!!!'
-#       Reactabular::Table.Provider( class: "pure-table pure-table-striped", columns: state.columns.to_n) {
-#         Reactabular::Table.Body( rows: state.rows.to_n, row_key: "id") }
-#     }
-#   end
-# end
